f88e316d57c916609834121a85ef26f7fea32be4
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-24 18:32:11 +0100

    Adding logs

--- simple-frontend/src/App.jsx
+++ simple-frontend/src/App.jsx
@@ -6,10 +6,10 @@ import './App.css'
 function App() {
   const [message, setMessage] = useState("");
   function fetchMessage(){
-    // console.log(process.env.BACKEND_URL);
-    // console.log(import.meta.env.VITE_BACKEND_URL);
-    // console.log(process.env.VITE_BACKEND_URL);
-    // console.log(import.meta.env.BACKEND_URL);
+    console.log(process.env.BACKEND_URL);
+    console.log(import.meta.env.VITE_BACKEND_URL);
+    console.log(process.env.VITE_BACKEND_URL);
+    console.log(import.meta.env.BACKEND_URL);
     fetch(process.env.BACKEND_URL + "/api/hello")
       .then((response) => response.text())
       .then((data) => setMessage(data))
