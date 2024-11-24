8c6848b8bdf9374dd03271f6e02d57c541f90bed
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-24 11:17:58 +0100

    Changing Backend path in frontend

--- simple-frontend/src/App.jsx
+++ simple-frontend/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   onst [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("https://https://orange-tree-06961b703.5.azurestaticapps.net/api/hello")
+    fetch("https://orange-tree-06961b703.5.azurestaticapps.net/api/hello")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error("Error fetching data", error));
