6e46c41207b432b507343fb19da3b7221143adae
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-24 18:19:11 +0100

    Adding log statements

--- simple-frontend/src/App.jsx
+++ simple-frontend/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   function fetchMessage(){
     console.log(process.env.BACKEND_URL);
     console.log(import.meta.env.VITE_BACKEND_URL);
+    console.log(process.env.VITE_BACKEND_URL);
+    console.log(import.meta.env.BACKEND_URL);
     fetch(process.env.BACKEND_URL + "/api/hello")
       .then((response) => response.text())
       .then((data) => setMessage(data))
