327702079d1fdad47c82c2de0e1a6ca0e1b883c2
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-24 11:06:05 +0100

    Changing function

--- backend/api/hello/index.js
+++ backend/api/hello/index.js
@@ -1,20 +1,30 @@
-  const express = require('express');
-  const app = express();
+  // const express = require('express');
+  // const app = express();
   
-  app.get('/api/hello', (req, res) => {
-      res.json({ message: 'Hello from Node.js API!' });
-  });
+  // app.get('/api/hello', (req, res) => {
+  //     res.json({ message: 'Hello from Node.js API!' });
+  // });
   
-  module.exports = async function (context, req) {
-    context.res = {
-      status: 200,
-      body: 'Hello from Node.js API!'
-    };
-  };
+  // module.exports = async function (context, req) {
+  //   context.res = {
+  //     status: 200,
+  //     body: 'Hello from Node.js API!'
+  //   };
+  // };
 
-  const port = process.env.PORT || 8080;
-  app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-  });
+  // const port = process.env.PORT || 8080;
+  // app.listen(port, () => {
+  //     console.log(`Server running on port ${port}`);
+  // });
 
-  module.exports = { app};
+  // module.exports = { app};
+
+  const { app } = require('@azure/functions');
+
+app.http('hello', {
+    methods: ['POST', 'GET'],
+    handler: async (request, context) => {
+        context.log('Http function was triggered.');
+        return { body: 'Hello, world!' };
+    }
+});
