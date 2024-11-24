7fb8ba426e1f29434504df9c8eade8999d80b777
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-24 19:01:28 +0100

    Adding correct backend url

--- simple-frontend/src/App.jsx
+++ simple-frontend/src/App.jsx
@@ -6,13 +6,7 @@ import './App.css'
 function App() {
   const [message, setMessage] = useState("");
   function fetchMessage(){
-    console.log(import.meta.env.VITE_BACKEND_URL);
-    console.log(import.meta.env.BACKEND_URL);
-    
-    console.log(process.env.BACKEND_URL);
-    console.log(process.env.VITE_BACKEND_URL);
-
-    fetch(process.env.BACKEND_URL + "/api/hello")
+    fetch(import.meta.env.VITE_BACKEND_URL + "/api/hello")
       .then((response) => response.text())
       .then((data) => setMessage(data))
       .catch((error) => console.error("Error fetching data", error));
